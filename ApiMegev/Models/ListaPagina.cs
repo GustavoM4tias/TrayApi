50cdd938b5c1e047c0ac76facd33d41ab1d28015
GustavoM4tias <128002171+GustavoM4tias@users.noreply.github.com>
2024-08-21 18:43:37 +0000

    delete all

--- ApiMegev/Models/ListaPagina.cs
+++
@@ -1,8 +0,0 @@
-﻿public class ListaPagina<T>
-{
-    public int Total { get; set; }
-    public int PageSize { get; set; }
-    public int CurrentPage { get; set; }
-    public int TotalPages { get; set; }
-    public List<T> Items { get; set; }
-}
